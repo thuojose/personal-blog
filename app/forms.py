b49500056d69666352f7f7207420294d94bac578
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-09-26 16:38:30 +0300

    added class for form registration in form.py

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,28 @@
-from flask_mail import Message
-from flask import render_template
-from . import mail
-subject_pref = 'New Blog'
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
-def mail_message(subject, template, to, **kwargs):
-    sender_email = 'josephmugus16@gmail.com'
 
-    email = Message(subject, sender = sender_email, recipients = [to])
-    email.body = render_template(template + ".txt", **kwargs)
-    email.html = render_template(template + ".html", **kwargs)
-    mail.send(email)
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
