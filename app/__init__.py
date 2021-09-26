7c92dcfef3167385b705bf54b84771c6ac19868f
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-09-26 13:55:09 +0300

    different installations

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,50 @@
-from flask import Flask
+from flask import Flask,render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy 
+from . import main
+from ..models import Blog
+from ..forms import CommentForm,Blogpost, PostForm
+from flask_login import login_required, current_user
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://khalyz:1234@localhost/blog'
+
+
+db = SQLAlchemy(app)
+
+
+@main.route('/blogs')
+@login_required
+def blogs():
+    all_blogs = Blog.query.order_by(db.desc(Blog.created_at)).limit(15)
+
+    return render_template('blogs.html', all_blogs=all_blogs)
+
+@main.route('/post/<int:post_id>')
+@login_required
+def post(post_id):
+    post = Blogpost.query.filter_by(id=post_id).first()
+
+    return render_template('post.html', post=post)
+
+@main.route('/post')
+def add():
+    return render_template('post.html')
+
+@main.route('/addblog', methods=['GET','POST'])
+@login_required
+def addpost():
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        new_blog = Blog(title=title,
+                        content=content, user=current_user)
+
+        new_blog.save_blog()
+        return redirect(url_for('main.index'))
+
+    return render_template('new_blog.html', form=form)
